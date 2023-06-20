a9cf3caa9c8bd19ff2c8c5cefe934a8b3808e9dc
leonardopea <leonardo.pea.1996@gmail.com>
2023-06-21 00:35:55 +0200

    header svolto con array  completato

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,98 @@
+<script>
+export default {
+    data(){
+        return{
+            links:[
+                {
+                    label: 'characters',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'comics',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'movies',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'tv',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'games',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'collectibles',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'videos',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'fans',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'news',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'shop',
+                    link: '#',
+                    active: false,
+                },
+            ]
+        }
+    }
+    
+}
+</script>
+<template>
+    <div>
+        <div class="jumbotron">
+            <img src="../../public/dc-logo.png" alt="">
+            <ul>
+                <li v-for="(link, index) in links" :key="index">
+                    <a :class="(link.active ? 'active' : '')" :href="link.link">{{ link.label }}</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+$dc_color:#0C7CEC;   // creo variabile per colore 
+
+
+    .jumbotron{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+    }
+
+    ul{
+        display: inline-block;
+
+        li{
+            display: inline-block;
+            margin-right: 20px;
+
+        }
+    }
+
+
+</style>
